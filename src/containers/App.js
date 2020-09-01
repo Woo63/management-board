daf20e6231c741f4090d6077b1ef6049bfcd9fbf
Woo63 <55555r@list.ru>
2020-09-01 19:51:25 +0400

    Creating HOC

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Board from "./Board";
 import Header from "../components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-        <Header title={'Project Management Board'}/>
-        <Board/>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const tables = [
+            {id: 1, title: 'TO DO'},
+            {id: 2, title: 'ACTIVE'},
+            {id: 3, title: 'REVIEW'},
+            {id: 4, title: 'DONE'}
+        ]
+        return (
+            <div className="App">
+                <Header title={'Project Management Board'}/>
+                <Board tables={tables}  dataSource={'../../assets/data.json'}/>
+            </div>
+        )
+    };
 }
 
 export default App;
